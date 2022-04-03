f79ce73e0a0faf0cd664c6789daa1d50fd68df2a
setaf007 <setaf007@hotmail.com>
2022-04-03 18:37:27 +0800

    included unary operators

--- Simple Pascal Interpreter/spi.py
+++ Simple Pascal Interpreter/spi.py
@@ -112,6 +112,11 @@ class BinOp(AST):
         self.token = self.op = op
         self.right = right
 
+class UnaryOp(AST):
+    def __init__(self, op, expr):
+        self.token = self.op = op
+        self.expr = expr
+
 class Num(AST):
     def __init__(self, token):
         self.token = token
@@ -138,9 +143,17 @@ class Parser(object):
             self.error()
 
     def factor(self):
-        #factor : INTEGER
+        #factor : (PLUS|MINUS) factor | INTEGER | LPAREN expr RPAREN
         token = self.current_token
-        if token.type == INTEGER:
+        if token.type == PLUS:
+            self.eat(PLUS)
+            node = UnaryOp(token, self.factor())
+            return node
+        elif token.type == MINUS:
+            self.eat(MINUS)
+            node = UnaryOp(token, self.factor())
+            return node       
+        elif token.type == INTEGER:
             self.eat(INTEGER)
             return Num(token)
         elif token.type == LPAREN:
@@ -213,6 +226,13 @@ class Interpreter(NodeVisitor):
             return self.visit(node.left) * self.visit(node.right)
         elif node.op.type == DIV:
             return self.visit(node.left) / self.visit(node.right)
+
+    def visit_UnaryOp(self, node):
+        op = node.op.type
+        if op == PLUS:
+            return +self.visit(node.expr)
+        elif op == MINUS:
+            return -self.visit(node.expr)
 
     def visit_Num(self, node):
         return node.value
