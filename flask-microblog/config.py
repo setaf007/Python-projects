e6d1fef1d5fc222f95b4e8ce3a07ce2ba798b657
setaf007 <setaf007@hotmail.com>
2022-04-13 21:16:36 +0800

    added flask-sqlalchemy db

--- flask-microblog/config.py
+++ flask-microblog/config.py
@@ -1,6 +1,12 @@
 #configuration variables for flask project set here
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #impt: value of secret key used as cryptographic key, useful to gen signatures or tokens
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    #fallback default for database of app.db if DATABASE_URL not set
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    #TRACK_MODIFICATIONS false as not needed (sends signal to application everytime a change is about to be made to db)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
--- flask-microblog/microblog.py
+++ flask-microblog/microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+#allows working with database entitites without importing on shell (flask shell)
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
